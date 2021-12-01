679731a9659e4efb52f6f1a536b6b02d3d8f0f82
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-01 08:08:54 +0100

    Solve Day01 part2

--- aoc/day01.jl
+++ aoc/day01.jl
@@ -5,4 +5,10 @@ function main()
     return sum([data[i] > data[i-1] for i = 2:length(data)])
 end
 
+function main2()
+    data = readdlm("day01.txt")
+    return sum([sum(data[i-2:i]) > sum(data[i-3:i-1]) for i = 4:length(data)])
+end    
+
 println(main())
+println(main2())
