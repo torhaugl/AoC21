d976f7bb73fda51da90e67477322ff77a2296a29
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-02 06:06:34 +0100

    Update day2

--- aoc/day02.jl
+++ aoc/day02.jl
@@ -1,46 +1,38 @@
 using DelimitedFiles
 using Test
 
-test_input = "aoc/day02_test.txt"
-input = "aoc/day02.txt"
+test_input = readdlm("aoc/day02_test.txt")
+input = readdlm("aoc/day02.txt")
 
-function solve1(fname)
-    horizontal = 0
-    depth = 0
-    for line in eachline(fname)
-        cmd, val = split(line)
-        val = parse(Int, val)
+function solve1(commands, values)
+    boat = [0,0]
+    for (cmd, val) in zip(commands, values)
         if (cmd == "forward")
-            horizontal += val
+            boat += [val, 0]
         elseif (cmd == "down")
-            depth += val
+            boat += [0, val]
         elseif (cmd == "up")
-            depth -= val
+            boat += [0, -val]
         end
     end
-    return depth*horizontal
+    return prod(boat)
 end
 
-function solve2(fname)
-    horizontal = 0
-    depth = 0
-    aim = 0
-    for line in eachline(fname)
-        cmd, val = split(line)
-        val = parse(Int, val)
+function solve2(commands, values)
+    boat = [0,0,0]
+    for (cmd, val) in zip(commands, values)
         if (cmd == "forward")
-            horizontal += val
-            depth += aim * val
+            boat += [val, boat[3]*val, 0]
         elseif (cmd == "down")
-            aim += val
+            boat += [0, 0, val]
         elseif (cmd == "up")
-            aim -= val
+            boat += [0, 0, -val]
         end
     end
-    return depth*horizontal
+    return boat[1] * boat[2]
 end
 
-@test solve1(test_input) == 150
-@test solve2(test_input) == 900
-println(solve1(input))
-println(solve2(input))
+@test solve1(test_input[:,1], test_input[:,2]) == 150
+@test solve2(test_input[:,1], test_input[:,2]) == 900
+println(solve1(input[:,1], input[:,2]))
+println(solve2(input[:,1], input[:,2]))
