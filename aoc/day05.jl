aaa33a058e36ed66c25cd1cf41bedfed60d5a5de
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-05 06:53:05 +0100

    Update day5

--- aoc/day05.jl
+++ aoc/day05.jl
@@ -4,17 +4,24 @@ using Test
 nums = readdlm("aoc/day05.txt", ',', Int)
 nums_test = readdlm("aoc/day05_test.txt", ',', Int)
 
+function iter(i,j) 
+    sign(x) = (x >= 0) ? 1 : -1
+    if i == j
+        Iterators.repeated(i)
+    else
+        i:sign(j - i):j
+    end
+end
+
 function solve1(nums)
-    x1, y1, x2, y2 = nums[:,1] .+1, nums[:,2] .+1, nums[:,3] .+1, nums[:,4] .+1
+        x1, y1, x2, y2 = nums[:,1] .+1, nums[:,2] .+1, nums[:,3] .+1, nums[:,4] .+1
 
     board = zeros(maximum(nums)+1, maximum(nums)+1)
-    for i = 1:length(x1)
-        a = min(x1[i], x2[i])
-        b = max(x1[i], x2[i])
-        c = min(y1[i], y2[i])
-        d = max(y1[i], y2[i])
+    for (a,b,c,d) = zip(x1, x2, y1, y2)
         if (a == b || c == d)
-            board[a:b, c:d] .+= 1
+            for (i,j) in zip(iter(a,b), iter(c,d))
+                board[i,j] += 1
+            end
         end
     end
     return count(>=(2), board)
@@ -24,30 +31,9 @@ function solve2(nums)
     x1, y1, x2, y2 = nums[:,1] .+1, nums[:,2] .+1, nums[:,3] .+1, nums[:,4] .+1
 
     board = zeros(maximum(nums)+1, maximum(nums)+1)
-    for i = 1:length(x1)
-        a = min(x1[i], x2[i])
-        b = max(x1[i], x2[i])
-        c = min(y1[i], y2[i])
-        d = max(y1[i], y2[i])
-        if (a == b)
-            board[a:b, c:d] .+= 1
-        elseif (c == d)
-            board[a:b, c:d] .+= 1
-        else
-            if x1[i] < x2[i]
-                xs = x1[i]:x2[i]
-            else
-                xs = x1[i]:-1:x2[i]
-            end
-            if y1[i] < y2[i]
-                ys = y1[i]:y2[i]
-            else
-                ys = y1[i]:-1:y2[i]
-            end
-
-            for (i,j) in zip(xs, ys)
-                board[i,j] += 1
-            end
+    for (a,b,c,d) = zip(x1, x2, y1, y2)
+        for (i,j) in zip(iter(a,b), iter(c,d))
+            board[i,j] += 1
         end
     end
     return count(>=(2), board)
