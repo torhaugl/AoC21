93a6fd90169343557e394fe25cd8b59d6e506747
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-06 07:12:41 +0100

    Day6

---
+++ aoc/day06.jl
@@ -0,0 +1,20 @@
+using DelimitedFiles
+using OffsetArrays
+using Test
+
+nums = readdlm("aoc/day06.txt", ',', Int)
+nums_test = readdlm("aoc/day06_test.txt", ',', Int)
+
+function solve(nums, days)
+    fish = [count(==(i), nums) for i = 0:8]
+    fish = OffsetArray(fish, 0:8)
+    for _ = 1:days
+        fish = circshift(fish, -1)
+        fish[6] += fish[8]
+    end
+    return sum(fish)
+end
+
+@test solve(nums_test, 18) == 26
+println(solve(nums, 80))
+println(solve(nums, 256))
