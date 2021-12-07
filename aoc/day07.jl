ce266fcb837906cbea12369675c13fe9bc1562af
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-07 06:49:32 +0100

    Day 07

---
+++ aoc/day07.jl
@@ -0,0 +1,20 @@
+using DelimitedFiles
+using Test
+using Statistics
+
+nums = readdlm("aoc/day07.txt", ',', Int)
+nums_test = readdlm("aoc/day07_text.txt", ',', Int)
+
+solve1(x) = x .- median(x) .|> abs |> sum
+
+function solve2(x)
+    cost(v) = map(n -> sum(1:abs(n)), v) |> sum |> Int
+    return [cost(x .- i) for i = minimum(x):maximum(x)] |> minimum
+end
+
+#@test solve(nums_test, 18) == 26
+@test solve1(nums_test) == 37
+println(solve1(nums))
+
+@test solve2(nums_test) == 168
+display(solve2(nums))
