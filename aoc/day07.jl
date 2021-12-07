e3644ff28389a4506f6f3199f816114b18a758c9
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-07 15:01:40 +0100

    Beautiful day7

--- aoc/day07.jl
+++ aoc/day07.jl
@@ -5,12 +5,9 @@ using Statistics
 nums = readdlm("aoc/day07.txt", ',', Int)
 nums_test = readdlm("aoc/day07_text.txt", ',', Int)
 
-solve1(x) = x .- median(x) .|> abs |> sum
-
-function solve2(x)
-    cost(v) = map(n -> sum(1:abs(n)), v) |> sum |> Int
-    return [cost(x .- i) for i = minimum(x):maximum(x)] |> minimum
-end
+solve1(x) = x .- median(x) .|> ceil .|> abs |> sum |> Int
+sum_one_to_n(n) = n*(n+1)÷2
+solve2(x) = x .- mean(x) .|> round .|> abs .|> sum_one_to_n |> sum |> Int
 
 @test solve1(nums_test) == 37
 println(solve1(nums))
