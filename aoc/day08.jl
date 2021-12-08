f5bc40d4a2b0e729ea80ede5f08586b962145893
Tor S. Haugland <tor.s.haugland@ntnu.no>
2021-12-08 09:55:05 +0100

    Update day8

--- aoc/day08.jl
+++ aoc/day08.jl
@@ -1,72 +1,94 @@
-x = collect(eachline("aoc/day08.txt"))
-y = [split(x, "|") for x in x]
-z = [x[2] for x in y]
-d = [split(x, " ") for x in z]
-e = [length(x[j]) for x in d for j=1:length(d[1])]
-println(count(==(2), e))
-println(count(==(3), e))
-println(count(==(4), e))
-println(count(==(7), e))
+using Combinatorics
 
-test = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab"
-nums = "cdfeb fcadb cdfeb cdbaf"
-test = split(test, " ")
-nums = split(nums, " ")
+function solve1(input)
+    filter2347(x) = filter(y -> y ∈ [2, 3, 4, 7], x)
+    return [split(l, "|")[2] |> split .|> length |> filter2347 |> length for l in eachline(input)] |> sum
+end
+
+function solve2(input)
+    # Doesn't work
+    inp1 = [split(l, "|")[1] |> split for l in eachline(input)]
+    inp2 = [split(l, "|")[2] |> split for l in eachline(input)]
+    nums = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+    for (line1, line2) in zip(inp1, inp2)
+        global rline2 = line2
+        for p in permutations(["a", "b", "c", "d", "e", "f", "g"])
+            #global rline1 = [replace(l, p[1] => "a", p[2] => "b", p[3] => "c", p[4] => "d", p[5] => "e", p[6] => "f", p[7] => "g") for l in line1]
+            #global rline1 = [replace(l, p[1] => "a", p[2] => "b", "b" => "c", p[4] => "d", p[5] => "e", "e" => "f", p[7] => "g") for l in line1]
+            #println(p)
+            #println(sort(rline1))
+            println(sort(nums))
+            #println([a == b for (a, b) in zip(sort(rline1), sort(nums))])
+            break
+            #if all([a == b for (a, b) in zip(sort(rline1), sort(nums)])
+                #println("HEI")
+                #global rline2 = replace(line2, p[1] => "a", p[2] => "b", p[3] => "c", p[4] => "d", p[5] => "e", p[6] => "f", p[7] => "g")
+            #end
+        end
+        #println(rline1)
+        #println(rline2)
+        #[findfirst(==(word), nums)[1] for word in rline2]
+        break
+    end
+    return
+end
+#solve2("aoc/day08_test.txt")
 
 function line2nums(line, nums)
     line = [join(sort(collect(l))) for l in line]
     nums = [join(sort(collect(l))) for l in nums]
 
-    one   = line[findall(x -> length(x) == 2, line)]
-    four  = line[findall(x -> length(x) == 4, line)]
-    seven = line[findall(x -> length(x) == 3, line)]
-    eight = line[findall(x -> length(x) == 7, line)]
+    one   = line[findfirst(x -> length(x) == 2, line)]
+    four  = line[findfirst(x -> length(x) == 4, line)]
+    seven = line[findfirst(x -> length(x) == 3, line)]
+    eight = line[findfirst(x -> length(x) == 7, line)]
 
-    right = one[1]
-    top = replace(replace(seven[1], right[1] => ""), right[2] => "")
-    middleleft = replace(replace(four[1], right[1] => ""), right[2] => "")
-    bottom = replace(replace(replace(replace(replace(eight[1], right[1] => ""),
+    right = one
+    top = replace(replace(seven, right[1] => ""), right[2] => "")
+    middleleft = replace(replace(four, right[1] => ""), right[2] => "")
+    bottom = replace(replace(replace(replace(replace(eight, right[1] => ""),
                                                                right[2] => ""),
                                                                top[1] => ""),
                                                                middleleft[1] => ""),
                                                                middleleft[2] => "")
-    #println(top)
-    #println(middleleft)
-    #println(bottom)
 
-    two = line[findall(x -> (length(x) == 5 &&
+    two = line[findfirst(x -> (length(x) == 5 &&
                     !(right[1] ∈ x && right[2] ∈ x) &&
                     !(middleleft[1] ∈ x && middleleft[2] ∈ x) &&
                     (bottom[1] ∈ x && bottom[2] ∈ x) &&
                     top[1] ∈ x), line)]
-    three = line[findall(x -> (length(x) == 5 &&
+    three = line[findfirst(x -> (length(x) == 5 &&
                     right[1] ∈ x && right[2] ∈ x &&
                     !(middleleft[1] ∈ x && middleleft[2] ∈ x) &&
                     !(bottom[1] ∈ x && bottom[2] ∈ x) &&
                     top[1] ∈ x), line)]
-    five = line[findall(x -> (length(x) == 5 &&
-                    x != two[1] && x != three[1]), line)]
-    six = line[findall(x -> (length(x) == 6 && !(right[1] ∈ x && right[2] ∈ x)), line)]
-    nine = line[findall(x -> (length(x) == 6 && !(bottom[1] ∈ x && bottom[2] ∈ x)), line)]
-    zero = line[findall(x -> (length(x) == 6 && x != six[1] && x != nine[1]), line)]
+    five = line[findfirst(x -> (length(x) == 5 &&
+                    x != two && x != three), line)]
+    six = line[findfirst(x -> (length(x) == 6 && !(right[1] ∈ x && right[2] ∈ x)), line)]
+    nine = line[findfirst(x -> (length(x) == 6 && !(bottom[1] ∈ x && bottom[2] ∈ x)), line)]
+    zero = line[findfirst(x -> (length(x) == 6 && x != six && x != nine), line)]
 
-    map2num = Dict(one[1] => 1, 
-                   two[1] => 2,
-                   three[1] => 3,
-                   four[1] => 4,
-                   five[1] => 5,
-                   six[1] => 6,
-                   seven[1] => 7,
-                   eight[1] => 8,
-                   nine[1] => 9,
-                   zero[1] => 0)
-    return ([get(map2num, num, 110000000000) for num in nums])
+    map2num = Dict(one => 1, 
+                   two => 2,
+                   three => 3,
+                   four => 4,
+                   five => 5,
+                   six => 6,
+                   seven => 7,
+                   eight => 8,
+                   nine => 9,
+                   zero => 0)
+    return ([getindex(map2num, num) for num in nums])
 end
-line2nums(test, nums)
 
-x = collect(eachline("aoc/day08.txt"))
-y = [split(x, "|") for x in x]
-lines = [split(x[1], " ") for x in y]
-numss = [split(x[2], " ") for x in y]
-d = [line2nums(line, nums[2:end]) for (line, nums) in zip(lines, numss)];
-println([v * 10^(4 - i) for (i, v) in enumerate(sum(d))] |> sum)
+function solve2(input)
+    x = collect(eachline(input))
+    y = [split(x, "|") for x in x]
+    lines = [split(x[1], " ") for x in y]
+    numss = [split(x[2], " ") for x in y]
+    d = [line2nums(line, nums[2:end]) for (line, nums) in zip(lines, numss)];
+    return [v * 10^(4 - i) for (i, v) in enumerate(sum(d))] |> sum
+end
+
+println(solve1("aoc/day08.txt"))
+println(solve2("aoc/day08.txt"))
